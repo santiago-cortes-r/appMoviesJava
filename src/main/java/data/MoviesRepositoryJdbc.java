55552a3747fec9fa406871c9062885656b7ea65c
santiago-cortes <santiagocortes202@gmail.com>
2025-06-09 16:18:40 -0500

    Implementation interface MoviesRepository in MoviesRepositoryJdbc, develop of findAll and test

---
+++ src/main/java/data/MoviesRepositoryJdbc.java
@@ -0,0 +1,45 @@
+package data;
+
+import models.Genre;
+import models.Movie;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+
+public class MoviesRepositoryJdbc implements MoviesRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public MoviesRepositoryJdbc(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Movie findById(long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Movie> findAll() {
+
+
+        return jdbcTemplate.query("select * from movies", movieMapper);
+    }
+
+    @Override
+    public void saveOrUpdate(Movie movie) {
+
+    }
+
+    private static RowMapper<Movie> movieMapper = (rs, rowNum) ->
+            new Movie(
+                    rs.getInt("id"),
+                    rs.getString("name"),
+                    rs.getInt("minutes"),
+                    Genre.valueOf(rs.getString("genre")));
+}
